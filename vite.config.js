1b3f51dcf46fcf5fb535a1334402c652a88ccf9e
YallaNuthan <yalla.nuthan06@gmail.com>
2025-07-12 16:52:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-<<<<<<< HEAD
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
-=======
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  root: ".",            // treat repo root as your project root
-  base: "/",            // ensure imports beginning with “/” resolve to root
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      // explicitly point Rollup at your index.html entry
-      input: path.resolve(__dirname, "index.html"),
-    },
-  },
-  resolve: {
-    alias: {
-      // let you import from “@/…” to “src/…”
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
+  base: '/',        // default; ensures /src imports work
   plugins: [react()],
-});
-
->>>>>>> 84bfba8ba7c78b70c185f607f2dcac9445fef8e7
+})
